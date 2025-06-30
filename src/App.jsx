31ccb61095367ded68419cd6fa9b181d106971d6
ferhan <kemriahusseingmail.com>
2025-06-30 16:10:18 +0300

    text decoration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 
-
+// node_modules\bootstrap\dist\css\bootstrap.min.css
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
 
 
   return (
+    <div className="card bg-info ">
+      <div className="card-title">
+        <h1 className='text-center p-4 text-white'>Hey first project </h1>
+      </div>
+      <div className="card-body">
+        <p className="text-white text-center" > hey jdj</p>
 
-    <div class>
-      <h1>Hey first project</h1>
+      </div>
     </div>
+
 
   )
 }
